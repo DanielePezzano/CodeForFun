30a8d65c3e91741159d033b91cd1c0fb52efb3a9
Daniele <daniele_pezzano@hotmail.com>
2021-11-25 14:25:50 +0100

    Add files via upload
    initial commit

---
+++ CodePlayGround/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodePlayGround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
